23ef79ccbae322ada1c92eba2eed0c5a8367b43b
John Doe <randomEme@protonmail.com>
2020-02-02 14:50:22 +0400

    Функция для отображения доски

--- main.js
+++ main.js
@@ -42,6 +42,7 @@ getInput()
   // console.log("Column: " + value);
   // console.log(value);
   console.log(isInputCorrect(value, board));
+  printBoard([["x", "o", null], [null, "o", null], ["x", null, null]]);
 });
 //user will input numbers from 1 instead of 0
 function getInput() {
@@ -72,4 +73,34 @@ function isInputCorrect(userInput, board){
   }
 
   return true;
+}
+
+//pretty print board
+function printBoard(board) {
+  //using string concat because console.log always puts a \n
+  let prettyPrint = "";
+  for(let i = 0; i < board.length; i++) {
+    prettyPrint += `\t${i}\t`;
+    if(board.length - i !== 1) {
+      prettyPrint += "|";
+    }
+  }
+  prettyPrint += "\n";
+  for(let i = 0; i < board.length; i++) {
+    prettyPrint += "-".repeat(17);
+  }
+  prettyPrint += "\n";
+  for(let i = 0; i < board.length; i++) {
+    prettyPrint += `${i}|`;
+    for(let j = 0; j < board[i].length; j++) {
+      let value = board[i][j] === null ? "\t\t|" : `\t${board[i][j]}\t|`;
+      prettyPrint += value;
+    }
+    prettyPrint += "\n";
+    for(let j = 0; j < board.length; j++) {
+      prettyPrint += "-".repeat(17);
+    }
+    prettyPrint += "\n";
+  }
+  console.log(prettyPrint);
 }
