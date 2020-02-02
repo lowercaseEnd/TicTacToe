f10e808c9606b4b194f330f267b05701f0426f11
John Doe <randomEme@protonmail.com>
2020-02-02 14:21:31 +0400

    Написал функцию для проверки введёных данных

--- main.js
+++ main.js
@@ -26,26 +26,50 @@ const rl = readline.createInterface({
   input: process.stdin,
   output: process.stdout
 });
-console.log(initBoard());
+let board = initBoard();
 getInput()
-.then((value) => {
-  console.log("Row: " + value);
-  return new Promise((resolve, reject) => {
+.then((row) => {
+  // console.log("Row: " + row);
+  return new Promise((resolve) => {
     rl.resume();
-    rl.question("Enter column: ", (value) => {
+    rl.question("Enter column: ", (column) => {
       rl.close();
-      resolve(value);
+      resolve([row, column]);
   })
 })
 })
 .then((value) => {
-  console.log("Column: " + value);
+  // console.log("Column: " + value);
+  // console.log(value);
+  console.log(isInputCorrect(value, board));
 });
+//user will input numbers from 1 instead of 0
 function getInput() {
-  return new Promise ((resolve, reject) => {
-    rl.question("Enter row: ", (value) => {
+  return new Promise ((resolve) => {
+    rl.question("Enter row: ", (row) => {
       rl.pause();
-      resolve(value)
+      resolve(row)
     });
   });
+}
+
+//check for end of the game
+function isGameFinished() {
+
+}
+
+//validate user input
+function isInputCorrect(userInput, board){
+  //check for valid number
+  let numberRow = +userInput[0];
+  let numberColumn = +userInput[1];
+  if(isNaN(numberColumn) || isNaN(numberRow)) {
+    return false;
+  }
+  //check if numbers are in range
+  if(numberRow - 1 >= board.length || numberColumn - 1 >= board[0].length) {
+    return false;
+  }
+
+  return true;
 }
